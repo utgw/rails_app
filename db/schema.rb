4652da14a88b00faa0bc88859feef4156316f9df
calkuno <clkn.xyz@gmail.com>
2023-01-03 20:54:03 +0900

    ✨ ログイン機能追加　

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema[7.0].define(version: 2023_01_06_053748) do
   create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -19,9 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_053748) do
     t.datetime "updated_at", null: false
   end
 
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_103754) do
->>>>>>> 40acbf5 (first commit)
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
