a1182861bfed17506b4e01e2eefe438d9328d8c5
calkuno <utgw.1202@gmail.com>
2023-01-08 17:30:57 +0900

    create an app.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_01_06_053748) do
   create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
