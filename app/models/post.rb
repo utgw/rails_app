753ee11014458ed92a79f2e24229cbc9a7de1c37
calkuno <utgw.1202@gmail.com>
2023-01-11 13:34:12 +0900

    ♻️ postモデルとuserモデルの関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 140}}
     validates :user_id, {presence: true}
 
-    def user
-      return User.find_by(id: self.user_id)
-    end
-  end
+    belongs_to :user
+end
   
