ba70ddd5dd5fe2b03681b7b5e7df1275c547378f
calkuno <utgw.1202@gmail.com>
2023-01-21 22:43:32 +0900

    ユーザーのemailカラムにunique indexを設定。 string型の制限を191文字に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_012345) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_091030) do
   create_table "likes", charset: "utf8mb4", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_012345) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
